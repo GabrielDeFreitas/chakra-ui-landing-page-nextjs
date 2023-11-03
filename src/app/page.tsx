07d7b8f12f888d298f122b65360f0515472ad401
gabriel freitas <gabrielfreitas.ilustra@gmail.com>
2023-11-03 13:59:52 -0300

    create Features and Testimonials

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,13 @@
+import Features from "@/components/Features";
 import HeroSection from "@/components/HeroSection";
+import Testimonials from "@/components/Testimonials";
 
 export default function Home() {
   return (
     <>
       <HeroSection/>
+      <Features/>
+      <Testimonials/>
     </>
   )
 }
