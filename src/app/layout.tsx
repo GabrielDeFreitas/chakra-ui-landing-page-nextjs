62baaf56b82bb6684704fbb6a1a73fe0c6e6b615
gabriel freitas <gabrielfreitas.ilustra@gmail.com>
2023-11-03 13:48:51 -0300

    create NavBar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import { ChakraProvider } from '@chakra-ui/react'
+import Navbar from '@/components/NavBar'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -18,6 +19,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <ChakraProvider>
+          <Navbar/>
           {children}
         </ChakraProvider>
       </body>
