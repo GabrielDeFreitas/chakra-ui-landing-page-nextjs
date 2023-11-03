4d40191538cd1a046b67cabded9484e537bb729c
gabriel freitas <gabrielfreitas.ilustra@gmail.com>
2023-11-03 18:10:50 -0300

    next metadatas

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,18 @@ import Features from "@/components/Features";
 import Footer from "@/components/Footer";
 import HeroSection from "@/components/HeroSection";
 import Testimonials from "@/components/Testimonials";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  description: 'A simple study project use Next.js and Chakra UI.',
+  openGraph: {
+    images: ['/public/pageImage.PNG'],
+    authors: ['Gabriel Freitas']
+  },
+  twitter: {
+    images: ['/public/pageImage.PNG'],
+  }
+};
 
 export default function Home() {
   return (
